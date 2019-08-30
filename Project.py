84d579ca310333e4cc06fbb85993cfd47e485c8c
xxyushxx <54439772+xxyushxx@users.noreply.github.com>
2019-08-30 15:31:35 +1000

    Update Project.py

--- Project.py
+++ Project.py
@@ -14,18 +14,19 @@ class ClientThread(threading.Thread):
         print("Connection from : ", addr)
         while True:
             try:
-                message = self.csocket.recv(1024)  # Fill in start          #Fill in end
+                message = self.csocket.recv(1024)  # this line is to transmit the request message from the client
                 message = message.decode()
                 #if msg == 'bye':                   #Placeholders as they don't work for html Files
                     #break
                 print(message)
-                filename = message.split()[1]
+                filename = message.split()[1] #contain the 2nd point of the header of the HTTP found by [1]
                 
-                f = open(filename[1:])
+                f = open(filename[1:]) # because the second part of the HTTP header includes a '/', this instructs to read from the second character expressed through '[1:]'                      !
+
                 outputdata = f.read()  # Stores the file content in a temporary filestate
 
                 self.csocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())  # Sends a HTTP header line
-                for i in range(0, len(outputdata)):
+                for i in range(0, len(outputdata)): #send content to the client
                     self.csocket.send(outputdata[i].encode())  # send content to the client
 
                 self.csocket.send("\r\n".encode())
@@ -33,12 +34,12 @@ class ClientThread(threading.Thread):
 
             except IOError:
 
-                self.csocket.send('HTTP/1.1 404 Not Found\r\n<'.encode()) # if error socket 
+                self.csocket.send('HTTP/1.1 404 Not Found\r\n<'.encode()) # if error in the socket 
                 self.csocket.send('<html><head></head><body><h1>404 Not Found<h1></body></html>\r\n'.encode())
                 print("Socket Error and Data Sent")
         self.csocket.close() #closes the socket
         print("Client at ", addr, " disconnected...")
-        sys.exit() # Terminate the program after sending the corresponding data has to go somewhere
+        sys.exit() # Terminate the program after the data is sent 
 
 
 
