ad497c15aa574e9dc2843a7b453fa7e2afea4e62
Dogry7 <40050014+Dogry7@users.noreply.github.com>
2019-09-05 00:35:21 +1000

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,69 +1,48 @@
-# import socket module
+#import socket module
 from socket import *
-import sys, threading  # In order to terminate the program
-
-
-# Class to create a new thread for each client socket connection
-class ClientThread(threading.Thread):
-    def __init__(self, address, clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print("\r\nNew connection added: ", address)
-
-    def run(self):
-        print("Connection from : ", addr)
-        while True:
-            try:
-                message = self.csocket.recv(1024)  # Fill in start          #Fill in end
-                message = message.decode()
-                #if msg == 'bye':                   #Placeholders as they don't work for html Files
-                    #break
-                print(message)
-                filename = message.split()[1]
-                
-                f = open(filename[1:])
-                outputdata = f.read()  # Stores the file content in a temporary filestate
-
-                self.csocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())  # Sends a HTTP header line
-                for i in range(0, len(outputdata)):
-                    self.csocket.send(outputdata[i].encode())  # send content to the client
-
-                self.csocket.send("\r\n".encode())
-                print("Socket Recieved and Data Sent\r\n")
-
-            except IOError:
-
-                self.csocket.send('HTTP/1.1 404 Not Found\r\n<'.encode()) # if error socket 
-                self.csocket.send('<html><head></head><body><h1>404 Not Found<h1></body></html>\r\n'.encode())
-                print("Socket Error and Data Sent")
-        self.csocket.close() #closes the socket
-        print("Client at ", addr, " disconnected...")
-        sys.exit() # Terminate the program after sending the corresponding data has to go somewhere
-
-
+import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-
-# Prepare a server socket
+#Prepare a sever socket
 print('Creating Socket')
-
 SERVERPORT = 80
 print('Creating Bind')
+serverSocket.bind(('', SERVERPORT))
+serverSocket.listen(10)
+print('Listening For Connections')
 
-serverSocket.bind((gethostname(), SERVERPORT))
 
-print('Bind Created')
 
-print('Socket created')
-
-serverSocket.listen(1)
-print('Listening For Friends')
-
-print('Ready to serve...')
 while True:
-    # Establish the connection
-    # Written by Ryan, Tylar and Aayush
-
+    print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
-    newthread = ClientThread(addr, connectionSocket)
-    newthread.run()
+    print('Connection Initiated with: ', addr)
+    
+    try:
+
+        message = connectionSocket.recv(1024)
+        if message != '':
+            print('The Message is: ', message)
+            filename = message.split()[1]
+            print('File name is: ', filename)
+            f = open(filename[1:])
+            outputdata = f.read()
+
+            connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+
+            for i in range(0,len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+
+            print("Socket Recieved and Data Sent\r\n")
+            connectionSocket.send("\r\n".encode())
+
+    except IOError:
+
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
+
+        connectionSocket.send('<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n'.encode())
+
+        print("Socket Error and Data Sent")
+        connectionSocket.close
+
+serverSocket.close()
